d37122e7b03cc8e2ad98f318233ca43d64d77b11
gbavila7 <gabriel13avila09@gmail.com>
2025-09-11 20:22:23 -0300

    Add files via upload

---
+++ atividade01_gabriel_avila.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+		//declaração de variáveis e seus tipos
+		// String nome;
+		// int idade;
+		// double altura;
+        // boolean estudante;
+
+		// nome = "Ávila";
+		// idade = 16;
+		// altura = 1.70;
+		// estudante = true;
+        
+		// System.out.println(nome);
+        // System.out.println(idade);
+        // System.out.println(altura);
+        // System.out.println(estudante);
+
+        int A;
+        int B;
+        int C;
+        int D;
+        
+        A = 25;
+        B = A*3;
+        C = 10;
+        D = A/C;
+
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(C);
+        System.out.println(D);
+    }
+}
+
