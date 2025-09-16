fca8dd155e416c640217f296689c4c7b3cf625a4
gbavila7 <gabriel13avila09@gmail.com>
2025-09-15 21:41:28 -0300

    atividade02_gabriel_avila

---
+++ atividade02_gabriel_avila.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade02_gabriell_avila_lima;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade02_gabriell_avila_lima {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //System.out.println("Olá mundo!");
+        //System.out.print("Olá mundo!");
+        //System.out.println("Olá/Fulano/");
+        
+        //1
+        //Scanner entrada = new Scanner(System.in);
+        //double raio,area,circunf;
+        
+        //System.out.print("Digite o valor do raio do círculo:");
+        //raio = entrada.nextDouble();
+        
+        //area = Math.PI*(Math.pow(raio,2));
+    
+        //circunf = 2*Math.PI*raio;
+        
+        //System.out.println("Resultado área:"+area);
+        //System.out.println("Resultado circunferência:"+circunf);
+        
+        
+        //2
+        //Scanner entrada = new Scanner(System.in);
+        //double peso,altura,imc;
+        
+        //System.out.print("Digite o seu peso:");
+        //peso = entrada.nextDouble();
+        
+        //System.out.print("Digite sua altura:");
+        //altura = entrada.nextDouble();
+        
+        //imc = peso/(Math.pow(altura, 2));
+        //System.out.println("Resultado:"+imc);
+        
+        //3
+        //Scanner entrada = new Scanner(System.in);
+        //double cateto1,cateto2,hipotenusa;
+        
+        //System.out.print("Digite o cateto 1:");
+        //cateto1= entrada.nextDouble();
+        
+        //System.out.print("Digite o cateto 2:");
+        //cateto2= entrada.nextDouble();
+        
+        //hipotenusa = Math.sqrt(Math.pow(cateto1, 2)+ Math.pow(cateto2, 2));
+        //System.out.print("O resultado é: %.3f", hipotenusa);
+        
+        Scanner entrada = new Scanner(System.in);
+        double r,d,d_c,t_r;
+        
+        System.out.println("Digite um valor em real:");
+        r = entrada.nextDouble(); 
+        
+        d = 5.32;
+        
+        d_c = (int)(r/d);
+        System.out.println("Com essa quantia em reais, você pode comprar:" + d_c);
+      
+        t_r = r - (d_c * d);
+        System.out.println("Troco em reais:"+ t_r);
+    }
+}
