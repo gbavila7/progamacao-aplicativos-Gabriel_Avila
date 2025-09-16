61e2c16a41519e54de2bbd1bbcf7c653e3bb3c7a
gbavila7 <gabriel13avila09@gmail.com>
2025-09-16 20:19:17 -0300

    Update atividade02_gabriel_avila.java

--- atividade02_gabriel_avila.java
+++ atividade02_gabriel_avila.java
@@ -59,7 +59,7 @@ public class Atividade02_gabriell_avila_lima {
         
         //hipotenusa = Math.sqrt(Math.pow(cateto1, 2)+ Math.pow(cateto2, 2));
         //System.out.print("O resultado é: %.3f", hipotenusa);
-
+        
         //4
         //Scanner entrada = new Scanner(System.in);
         //double r,d,d_c,t_r;
@@ -68,13 +68,79 @@ public class Atividade02_gabriell_avila_lima {
         //r = entrada.nextDouble(); 
         
         //d = 5.32;
-        
-        //d_c = (int)(r/d);
-        //System.out.println("Com essa quantia em reais, você pode comprar:" + d_c);
-      
+        //d_c = Math.floor(r/d);
         //t_r = r - (d_c * d);
+          
+        //System.out.println("Com essa quantia em reais, você pode comprar:" + d_c);
         //System.out.println("Troco em reais:"+ t_r);
+        
+        //5
+        //Scanner entrada = new Scanner(System.in);
+        //int n,j1,j2,soma;
+        
+        //n = entrada.nextInt();
+        //j1 = entrada.nextInt();
+        //j2 = entrada.nextInt();
+        //soma = (j1+j2)/n;
+        
+        //System.out.println("A média é:"+soma);
+        //System.out.println(Math.max(j1, j2));
+        //System.out.println(Math.min(j1, j2));
+        
+        //6
+        //Scanner entrada = new Scanner(System.in);
+        //double angulo,tan,sen,cos,rad;
+        
+        //System.out.println("Insira o valor de um ângulo:");
+        //angulo = entrada.nextDouble();
+        
+        //rad = Math.toRadians(angulo);
+        //tan = Math.tan(rad);
+        //sen = Math.sin(rad);
+        //cos = Math.cos(rad);
+        
+        //System.out.println("Tangente desse ângulo:"+tan);
+        //System.out.println("Seno desse ângulo:"+sen);
+        //System.out.println("Cosseno desse ângulo:"+cos);
+        
+        //7
+        //Scanner entrada = new Scanner(System.in);
+        //double x1,x2,y1,y2,calc;
+        
+        //x1 = entrada.nextDouble();
+        //x2 = entrada.nextDouble();
+        //y1 = entrada.nextDouble();
+        //y2 = entrada.nextDouble();
+        
+        //calc = Math.sqrt(Math.pow((x2-x1), 2)) + (Math.pow((y1-y2), 2));
+        
+        //System.out.println("A distância euclediana é:" +calc);
+        
+        //8
+        //Scanner entrada = new Scanner(System.in);
+        //double n1,n2,n3,media,soma,dif;
+        
+        //n1 = entrada.nextDouble();        //7
+        //Scanner entrada = new Scanner(System.in);
+        //double x1,x2,y1,y2,calc;
+        
+        //x1 = entrada.nextDouble();
+        //x2 = entrada.nextDouble();
+        //y1 = entrada.nextDouble();
+        //y2 = entrada.nextDouble();
+        
+        //calc = Math.sqrt(Math.pow((x2-x1), 2)) + (Math.pow((y1-y2), 2));
+        
+        //System.out.println("A distância euclediana é:" +calc);
+        //n2 = entrada.nextDouble();
+        //n3 = entrada.nextDouble();
+        
+        //media = (n1+n2+n3)/3;
+        //soma = Math.abs(n1)+Math.abs(n2)+Math.abs(n3);
+        //dif = Math.abs(n1-n3);
+        
+        //System.out.println("A média é:"+media);
+        //System.out.println("A soma dos valores absolutos é:"+soma);
+        //System.out.println("A maior diferença é:"+dif);
     }
 }
-
-
