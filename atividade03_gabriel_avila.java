94910b4c4b24a67013fbc76ad4ce3aff45ef666c
gbavila7 <gabriel13avila09@gmail.com>
2025-09-16 21:21:44 -0300

    Add files via upload

---
+++ atividade03_gabriel_avila.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade03_gabriel_avila.java;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade03_gabriel_avilaJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //1
+        Scanner entrada = new Scanner(System.in);
+        int p, a;
+        
+        p = 8;
+        a = 200;
+        
+        System.out.print
+        p = entrada.nextDouble();
+        
+    }
+    
+}
