28ff889bc3728b5f3d0cad74c20cfaae0d9cda5b
gbavila7 <gabriel13avila09@gmail.com>
2025-09-18 19:21:57 -0300

    Add files via upload

---
+++ atividade04_gabriel_avila.java
@@ -0,0 +1,206 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade04_gabriel_avila;
+import java.util.Scanner;
+import java.lang.Math; 
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade04_gabriel_avila {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //1
+        //Scanner entrada = new Scanner(System.in);
+        //int num;
+        
+        //System.out.print("Insira um número:");
+        //num = entrada.nextInt();
+        
+        //if (num<0) {
+        //  System.out.print("Você digitou um número negativo.");
+        //}
+        
+        //else{
+        //  System.out.print("Você digitou um número positivo.");
+        //}
+        
+        //2
+        //Scanner entrada = new Scanner(System.in);
+        //int num;
+        
+        //System.out.print("Insira um número:");
+        //num = entrada.nextInt();
+        
+        //if (num<0) {
+        //  System.out.print("Você digitou um número negativo.");
+        //}
+        
+        //if (num>0) {
+        //  System.out.print("Você digitou um número positivo.");
+        //}
+        
+        //if (num==0) {
+        //  System.out.print("Você digitou zero.");
+        //}
+        
+        //3
+        //Scanner entrada = new Scanner(System.in);
+        //double nota1,nota2,nota3,media,soma;
+        
+        //System.out.print("Digite a primeira nota:");
+        //nota1 = entrada.nextDouble();
+        
+        //System.out.print("Digite a segunda nota:");
+        //nota2 = entrada.nextDouble();
+        
+        //System.out.print("Digite a terceira nota:");
+        //nota3 = entrada.nextDouble();
+        
+        //soma = nota1+nota2+nota3;
+        //media = soma/3;
+        
+        //if (media>=7) {
+        //  System.out.print("Aluno aprovado.");
+        //}
+        
+        //else {
+        //  System.out.print("Aluno reprovado.");
+        //}
+        
+        //4
+        //Scanner entrada = new Scanner(System.in);
+        //double nota1,nota2,nota3,media,soma;
+        
+        //System.out.print("Digite a primeira nota:");
+        //nota1 = entrada.nextDouble();
+        
+        //System.out.print("Digite a segunda nota:");
+        //nota2 = entrada.nextDouble();
+        
+        //System.out.print("Digite a terceira nota:");
+        //nota3 = entrada.nextDouble();
+        
+        //soma = nota1+nota2+nota3;
+        //media = soma/3;
+        
+        //if (media>=7) {
+        //  System.out.print("Aluno aprovado.");
+        //}
+        
+        //if (media>=5 && media<7) {
+        //  System.out.print("Aluno em recuperação.");
+        //}
+        
+        //if (media<5) {
+        //  System.out.print("Aluno reprovado.");
+        //}
+        
+        //5
+        //Scanner entrada = new Scanner(System.in);
+        //int num;
+        
+        //System.out.print("Insira um número:");
+        //num = entrada.nextInt();
+        
+        //if (num%2==0) {
+        //  System.out.print("O número é par.");
+        //}
+        
+        //else {
+        //  System.out.print("O número é ímpar.");
+        //}
+        
+        //6
+        //Scanner entrada = new Scanner(System.in);
+        //nt num1,num2,num3,maior;
+        
+        //System.out.print("Insira um número:");
+        //num1 = entrada.nextInt();
+        
+        //System.out.print("Insira outro número:");
+        //num2 = entrada.nextInt();        
+        
+        //System.out.print("Insira mais um número:");
+        //num3 = entrada.nextInt();
+        
+        //maior = Math.max(num1, Math.max(num2, num3));
+        
+        //System.out.print("O maior número é: "+maior);
+        
+        //7
+        //Scanner entrada = new Scanner(System.in);
+        //int i_nadador;
+        
+        //System.out.print("Insira a idade do nadador:");
+        //_nadador = entrada.nextInt();
+        
+        //if (i_nadador>=18){
+        //  System.out.print("Adulto.");
+        //}
+        
+        //if (i_nadador>=14 && i_nadador<18){
+        //  System.out.print("Juvenil.");
+        //}
+            
+        //if (i_nadador>=9 && i_nadador<14){
+        //  System.out.print("Infantil.");
+        //}
+            
+        //if (i_nadador<9){
+        //  System.out.print("Mirim.");
+        //}
+        
+        //8
+        //Scanner entrada = new Scanner(System.in);
+        //double num1,num2,soma;
+                
+        //System.out.print("Insira um número:");
+        //num1 = entrada.nextDouble();
+        
+        //System.out.print("Insira outro número:");
+        //num2 = entrada.nextDouble();   
+        
+        //soma = num1+num2;
+        
+        //if (soma>20) {
+        //    soma = soma+8;
+        //    System.out.print("Valor total:"+soma);
+        //}
+        
+        //if (soma<=20) {
+        //    soma = soma-5;
+        //    System.out.print("Valor total:"+soma);
+        //}
+          
+        //9
+        //Scanner entrada = new Scanner(System.in);
+        //double valorProduto,lucro1,lucro2,venda1,venda2;
+        
+        //System.out.print("Insira o valor do produto:");
+        //valorProduto = entrada.nextInt();
+        
+        //lucro1 = valorProduto * 0.45;
+        //lucro2 = valorProduto * 0.30;
+        //venda1 = valorProduto + lucro1;
+        //venda2 = valorProduto + lucro2;
+        
+        //if (valorProduto<20.00){
+        //    System.out.print("Valor do produto: "+valorProduto+", com o lucro incluso:"+venda1);
+        //}
+        
+        //else {
+        //    System.out.print("Valor do produto: "+valorProduto+", com o lucro incluso:"+venda2);
+        //}
+        
+        //10
+        Scanner entrada = new Scanner(System.in);
+        
+    }
+    
+}
