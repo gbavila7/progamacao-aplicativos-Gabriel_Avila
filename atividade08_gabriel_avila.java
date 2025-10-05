17757a70c0f8dd6081fd876514b936d8e9bee128
gbavila7 <gabriel13avila09@gmail.com>
2025-10-05 14:17:32 -0300

    Add files via upload

---
+++ atividade08_gabriel_avila.java
@@ -0,0 +1,94 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade08_gabriel_avila;
+import java.lang.Math;
+import java.util.Scanner;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade08_gabriel_avila {
+    //1
+    /*public double calcula_delta(int a,int b,int c){
+        double calc =(Math.pow(b,2)-(4*a*c));
+            return calc;
+    }*/
+    
+    //2
+    /*public int soma(int n1,int n2) {
+        return n1+n2;
+    }
+    public int subtracao(int y,int z) {
+        return y-z;
+    }
+
+    public int multiplicacao(int x,int n) {
+        return x*n;
+    }    
+
+    public double divisao(int a,int b) {
+        return a/b;
+    }*/    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+       
+        //1
+       /*Atividade08_gabriel_avila equacao = new Atividade08_gabriel_avila();
+       System.out.print("dada a equação de 2° grau -3x2+4x-2=0, qual o resultado para x1 e x2?");
+       double d=equacao.calcula_delta(-3,4,2);
+       double x1= -4+Math.sqrt(d)/2*(-3);
+       double x2= -4-Math.sqrt (d)/(2* (-3));*/
+        
+        //2
+        /*int opcao, num1, num2, y, z, x, n, a, b;
+        double resultado;
+        System.out.print("Calculadora \n1 - Soma \n2 - Subtração \n3 - Multiplicação \n4 - Divisão\n0 - Sair\n");
+        
+        System.out.print("Insira uma opção:");
+        opcao = entrada.nextInt();
+        
+        Atividade08_gabriel_avila calculadora = new Atividade08_gabriel_avila();
+        switch (opcao){
+            case 1:
+                System.out.print("Digite um número inteiro: ");
+                num1 = entrada.nextInt();
+                System.out.print("Digite outro número inteiro: ");
+                num2 = entrada.nextInt();
+                resultado = calculadora.soma(num1,num2);
+                System.out.printf("A soma de %d e %d é %.2f",num1,num2,resultado);
+            case 2:
+                System.out.print("Digite um número inteiro: ");
+                y = entrada.nextInt();
+                System.out.print("Digite outro número inteiro: ");
+                z = entrada.nextInt();
+                resultado = calculadora.subtracao(y, z);
+                System.out.printf("A subtração de %d e %d é %.2f",y,z,resultado);
+                
+            case 3:
+                System.out.print("Digite um número inteiro: ");
+                x = entrada.nextInt();
+                System.out.print("Digite outro número inteiro: ");
+                n = entrada.nextInt();       
+                resultado = calculadora.multiplicacao(x, n);
+                System.out.printf("A multiplicação de %d e %d é %.2f",x,n,resultado);
+                
+            case 4:
+                System.out.print("Digite um número inteiro: ");
+                a = entrada.nextInt();
+                System.out.print("Digite outro número inteiro: ");
+                b = entrada.nextInt();       
+                resultado = calculadora.divisao(a, b);
+                System.out.printf("A divisão de %d e %d é %.2f",a,b,resultado);
+                
+            case 0:
+                System.out.print("Encerrando o sistema.");
+        
+        //3
+        
+        }*/
+    }
+    
+}
